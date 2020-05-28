140add1f02315d66ed338d103a8ce44e4826b625
Jackson Cole <jackson@jacksoncole.io>
2020-05-28 01:31:12 -0500

    Add additional config
    Jackson Cole <jackson@jacksoncole.io>

--- config.def.h
+++ config.def.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+    "Hack Nerd Font Mono:pixelsize=22:antialias=true:autohint=true"
+};
+
 static int borderpx = 2;
 
 /*
