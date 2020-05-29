ca3c6cb33d9af2849a7e9e1b1f08cf5c8eea50fa
Jackson Cole <jackson.cole@apexturbine.com>
2020-05-29 05:29:46 -0500

    Change fontsize on work pc, may have to have separate config

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    "Hack Nerd Font Mono:pixelsize=22:antialias=true:autohint=true"
+    "Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true"
 };
 
 static int borderpx = 20;
